dca8f35426643e62428a8ad744e66e0e6be01aa1
Dulguun <dulguun.web@gmail.com>
2022-10-26 23:04:12 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        dark: "#181818",
+        primary: "#d29e0e",
+        secondary: "#e5001c",
+      },
+    },
+  },
+  plugins: [],
+};
